be4d67ac98455eed761208f9011a82929b0396cc
Teresa Toledo <laratoledoteresa@hotmail.com>
2024-01-26 19:48:40 +0100

    Add title

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,6 +13,11 @@ function App() {
         <img src={logo} alt="logo"/>
       </header>
       <main className="main">
+        <section className='titleSection'>
+          <h1 className='titleSection__h1'>Proyectos molones</h1>
+          <p className='titleSection__p'>Escaparate en línea para recoger ideas a través de la tecnología.</p>
+          <a className='titleSection__btn' href="#">Ver proyectos</a>
+        </section>
         <section className="preview">
           <img className="image" src={cover} alt="" />
 
