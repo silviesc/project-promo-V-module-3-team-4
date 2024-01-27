70456e8027e792f5d588cbea2e545f014c9338ee
natlopar <natalialopezariel@gmail.com>
2024-01-27 15:36:35 +0100

    mobile preview

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,30 +14,31 @@ function App() {
       </header>
       <main className="main">
         <section className="preview">
-          <img className="image" src={cover} alt="" />
+          <img className="image preview__image" src={cover} alt="" />
 
-          <section className="autor">
-            <section className="info-project">
-              <p className="subtitle">Personal Project Card</p>
-              <hr className="line" />
+          <section className="autor preview__autor">
+            <section className="info-project previewProject">
+              <p className="subtitle previewProject__subtitle">Personal Project Card</p>
+              <hr className="line previewProject__line" />
 
-              <h2 className="title">Elegant Workspace</h2>
-              <p className="slogan">Diseños Exclusivos</p>
-              <p className="desc">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Libero, delectus? Voluptates at hic aliquam porro ad suscipit
-                harum laboriosam saepe earum doloribus aperiam, ullam culpa
-                accusantium placeat odit corrupti ipsum!
+              <h2 className="previewProject__title">Elegant Workspace</h2>
+              <p className="slogan previewProject__slogan">Diseños Exclusivos</p>
+              <p className="previewProject__desc">
+                Product Description.
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae voluptas consequuntur tempore libero veritatis.
               </p>
-              <section className="technologies">
-                <p className="text">React JS, MongoDB</p>
+              <section className="technologies previewProject__technologies">
+                <p className="text previewProject__technologies--text">React JS, MongoDB</p>
+                <i class="fa-solid fa-globe previewProject__technologies--world"></i> 
+                <i className="fa-brands fa-github previewProject__technologies--github"></i>
+                      
               </section>
             </section>
 
-            <section className="info-autor">
-              <img className="image" src={user} alt="" />
-              <p className="job">Full Stack Developer</p>
-              <p className="name">Emmelie Björklund</p>
+            <section className="info-autor previewAutor">
+              <img className="image previewAutor__image" src={user} alt="" />
+              <p className="job previewAutor__job">Full Stack Developer</p>
+              <p className="name previewAutor__name">Emmelie Björklund</p>
             </section>
           </section>
         </section>
