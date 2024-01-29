87e87c6d90f881b805a5b49c282ea17901284cf4
Teresa Toledo <laratoledoteresa@hotmail.com>
2024-01-29 19:44:06 +0100

    Fix card inputs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,21 +16,23 @@ function App() {
   const [job, setJob] = useState('');
 
   const handleChange = (event) => {
-    if (event.target.id === name) {
+    
+    if (event.target.id === 'name') {
+      console.log(event.target.id)
       setProjectName(event.target.value);
-    } else if (event.target.id === slogan) {
+    } else if (event.target.id === 'slogan') {
       setSlogan(event.target.value);
-    } else if (event.target.id === repo) {
+    } else if (event.target.id === 'repo') {
       setRepository(event.target.value);
-    } else if (event.target.id === demo) {
+    } else if (event.target.id === 'demo') {
       setDemo(event.target.value);
-    } else if (event.target.id === technologies) {
+    } else if (event.target.id === 'technologies') {
       setTech(event.target.value)
-    } else if (event.target.id === desc) {
+    } else if (event.target.id === 'desc') {
       setDesc(event.target.value);
-    } else if (event.target.id === autor) {
+    } else if (event.target.id === 'autor') {
       setUserName(event.target.value);
-    } else if (event.target.id === job) {
+    } else if (event.target.id === 'job') {
       setJob(event.target.value);
     }
   }
