ad6b7642b918213299757be6ceabc6358006bc54
Silvia Escanciano <silvia.escanciano@gmail.com>
2024-01-31 15:55:49 +0100

    lifting and props

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,104 +1,97 @@
+const Form = ({ handleChange }) => {
+  // const handleForm = (ev) => {
+  //   const id = ev.target.id;
+  //   const value = ev.target.value;
+  //   handleChange(id, value);
+  // };
 
-const Form = ({projectName,slogan,repository,demo,tech,desc,userName,job, handleChange}) => {
-  const handleForm = (ev) =>{
-    const id = ev.target.id;
-    const value = ev.target.value;
-    handleChange (id,value);
-  }
-    return (
-        <section className="form" onChange={handleForm}>
-        <h2 className="form__title">Información</h2>
+  return (
+    <section className="form">
+      <h2 className="form__title">Información</h2>
 
-        <section className="form__askInfo">
-          <p className="form__askInfo--subtitle">Cuéntanos sobre el proyecto</p>
-          <hr className="form__askInfo--line" />
-        </section>
-
-        <fieldset className="form__project">
-          <input 
-            className="form__project--input"
-            type="text"
-            placeholder="Nombre del proyecto"
-            name={projectName}
-            id={projectName}
-          />
-          <input
-            className="form__project--input"
-            type="text"
-            name={slogan}
-            id={slogan}
-            placeholder="Slogan"
-          />
-          <input
-            className="form__project--input"
-            type="text"
-            name= {repository}
-            id={repository}
-            placeholder="Repo"
-          />
-          <input
-            className="form__project--input"
-            type="text"
-            placeholder="Demo"
-            name={demo}
-            id={demo}
-          />
-          <input
-            className="form__project--input"
-            type="text"
-            placeholder="Tecnologías"
-            name={tech}
-            id={tech}
-          />
-          <textarea
-            className="form__project--textarea"
-            type="text"
-            placeholder="Descripción"
-            name={desc}
-            id={desc}
-          ></textarea>
-        </fieldset>
-
-        <section className="form__askInfo">
-          <p className="form__askInfo--subtitle">Cuéntanos sobre la autora</p>
-          <hr className="form__askInfo--line" />
-        </section>
-
-        <fieldset className="form__autor">
-          <input
-            className="form__autor--input"
-            type="text"
-            placeholder="Nombre"
-            name={userName}
-            id={userName}
-          />
-          <input
-            className="form__autor--input"
-            type="text"
-            placeholder="Trabajo"
-            name={job}
-            id={job}
-          />
-        </fieldset>
-
-        <section className="form__buttonsImg">
-          <button className="form__buttonsImg--upload">Subir foto de proyecto</button>
-          <button className="form__buttonsImg--upload">Subir foto de autora</button>
-        </section>
-        <section className="form__buttonsImg">
-          {/*<button className="form__buttonsImg--create" onClick="{handleClickCreateCard}">
-            Crear Tarjeta
-          </button>*/}
-        </section>
-
-        <section className="form__card">
-          <span className="hidden"> La tarjeta ha sido creada: </span>
-          <a href="" className="" target="_blank" rel="noreferrer">
-            {" "}
-          </a>
-        </section>
+      <section className="form__askInfo">
+        <p className="form__askInfo--subtitle">Cuéntanos sobre el proyecto</p>
+        <hr className="form__askInfo--line" />
       </section>
-    )
-}
+
+      <fieldset className="form__project">
+        <input
+          className="form__project--input"
+          type="text"
+          placeholder="Nombre del proyecto"
+          name="name"
+          id="name"
+          onChange={(e) => handleChange(e.target.id, e.target.value)}
+        />
+        <input
+          className="form__project--input"
+          type="text"
+          name="slogan"
+          id="slogan"
+          placeholder="Slogan"
+          onChange={(e) => handleChange(e.target.id, e.target.value)}
+        />
+        <input
+          className="form__project--input"
+          type="text"
+          name="repository"
+          id="repository"
+          placeholder="Repo"
+          onChange={(e) => handleChange(e.target.id, e.target.value)}
+        />
+        <input
+          className="form__project--input"
+          type="text"
+          placeholder="Demo"
+          name="demo"
+          id="demo"
+          onChange={(e) => handleChange(e.target.id, e.target.value)}
+        />
+        <input
+          className="form__project--input"
+          type="text"
+          placeholder="Tecnologías"
+          name="tech"
+          id="tech"
+          onChange={(e) => handleChange(e.target.id, e.target.value)}
+        />
+        <textarea
+          className="form__project--textarea"
+          type="text"
+          placeholder="Descripción"
+          name="desc"
+          id="desc"
+          onChange={(e) => handleChange(e.target.id, e.target.value)}
+        ></textarea>
+      </fieldset>
+
+      <section className="form__askInfo">
+        <p className="form__askInfo--subtitle">Cuéntanos sobre la autora</p>
+        <hr className="form__askInfo--line" />
+      </section>
+
+      <fieldset className="form__autor">
+        <input
+          className="form__autor--input"
+          type="text"
+          placeholder="Nombre"
+          name="author"
+          id="author"
+          onChange={(e) => handleChange(e.target.id, e.target.value)}
+        />
+        <input
+          className="form__autor--input"
+          type="text"
+          placeholder="Trabajo"
+          name="job"
+          id="job"
+          onChange={(e) => handleChange(e.target.id, e.target.value)}
+        />
+      </fieldset>
+
+      {/* ... */}
+    </section>
+  );
+};
 
 export default Form;
