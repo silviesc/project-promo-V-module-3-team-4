e66e9a31aeb147f3c6d85d3d0c9e7d165d60564d
Teresa Toledo <laratoledoteresa@hotmail.com>
2024-02-05 13:24:08 +0100

    Add object and get avatar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,47 +1,37 @@
-import "../scss/App.scss";
-import { useState } from "react";
-import Header from "./Header";
-import Main from "./Main";
-import Footer from "./Footer";
+import '../scss/App.scss';
+import { useState } from 'react';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 
 function App() {
-  const [projectName, setProjectName] = useState("");
-  const [slogan, setSlogan] = useState("");
-  const [repository, setRepository] = useState("");
-  const [demo, setDemo] = useState("");
-  const [tech, setTech] = useState("");
-  const [desc, setDesc] = useState("");
-  const [userName, setUserName] = useState("");
-  const [job, setJob] = useState("");
-  const handleChange = (id,value) => {
-    if (id === 'name') {
-      setProjectName(value);
-    } else if (id === 'slogan') {
-      setSlogan(value);
-    } else if (id === 'repo') {
-      setRepository(value);
-    } else if (id === 'demo') {
-      setDemo(value);
-    } else if (id === 'tech') {
-      setTech(value);
-    } else if (id === 'desc') {
-      setDesc(value);
-    } else if (id === 'author') {
-      setUserName(value);
-    } else if (id === 'job') {
-      setJob(value);
-    }
-  };
+	const [dataCard, setDataCard] = useState({
+		name: '',
+		slogan: '',
+		technologies: '',
+		repo: '',
+		demo: '',
+		desc: '',
+		autor: '',
+		job: '',
+		image: '',
+		photo: '',
+	});
+	const handleInput = (name, value) => {
+		const inputValue = value;
+		const inputName = name;
+		setDataCard({ ...dataCard, [inputName]: inputValue });
+	};
 
-  return (
-    <>
-      <div className="container">
-        <Header/>
-        <Main projectName={projectName} slogan={slogan} repository={repository} demo={demo} tech={tech} desc={desc} userName ={userName} job={job} handleChange={handleChange}/>
-        <Footer/>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="container">
+				<Header />
+				<Main dataCard={dataCard} handleInput={handleInput} />
+				<Footer />
+			</div>
+		</>
+	);
 }
 
 export default App;
