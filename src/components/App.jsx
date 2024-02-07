0dd9c52749d6b2ca1d068b1c114b4983399008a5
Silvia Escanciano <silvia.escanciano@gmail.com>
2024-02-07 15:15:22 +0100

    no changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import "../scss/App.scss";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
